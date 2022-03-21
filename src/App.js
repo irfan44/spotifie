7d30f166fba06320bc176423c1184680a89d6084
irfan44 <irfannmuhajir@gmail.com>
2022-03-21 13:24:17 +0000

    Adding data and displaying to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import data from "./data/single-sample";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <img src={data.album.images[0].url} width="124" height="124" />
+      </div>
+      <div>
+        <h2>{data.name}</h2>
+        <h3>{data.album.name}</h3>
+        <p>{data.artists[0].name}</p>
+      </div>
     </div>
   );
 }
