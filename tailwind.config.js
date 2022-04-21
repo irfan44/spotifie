213bde3187a4d54034b7a0ceab4c3a2fce0bfb84
irfan44 <irfannmuhajir@gmail.com>
2022-04-21 13:44:16 +0000

    feat: add custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Plus Jakarta Sans', ...fontFamily.sans],
+      },
+    },
   },
-  plugins: [require("tailwind-scrollbar-hide")],
+  plugins: [require('tailwind-scrollbar-hide')],
 };
